b3a55382bd8020175020c67d8dea1e46932853e7
yingyingPengSAP <67150471+yingyingPengSAP@users.noreply.github.com>
2021-08-20 17:42:18 +0100

    Add files via upload

---
+++ kong/plugins/testgeneratejwt/schema.lua
@@ -0,0 +1,36 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+-- Grab pluginname from module name
+local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
+
+local schema = {
+  name = plugin_name,
+  fields = {
+    -- the 'fields' array is the top-level entry with fields defined by Kong
+    { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
+    { protocols = typedefs.protocols_http },
+    { config = {
+        -- The 'config' record is the custom part of the plugin schema
+        type = "record",
+        fields = {
+          { username = { -- self defined field
+              type = "string",
+              default = "",
+              required = true,
+            },
+            pass = { -- self defined field
+              type = "string",
+              default = "",
+              required = true,
+            }
+        }, -- adding a constraint for the value
+        },
+        entity_checks = {
+          -- add some validation rules across fields
+          -- the following is silly because it is always true, since they are both required
+          { at_least_one_of = { "request_header", "response_header" }, }
+        },
+      },
+    },
+  },
+}
